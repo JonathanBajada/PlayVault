4c284b517a4ee8aa9b4b722ffaec3be7458ebb4d
Jonatahn Bajada <jbajada@ryerson.ca>
2025-12-29 14:04:11 +0100

    adding basic crud functionality

--- index.ts
+++ index.ts
@@ -22,6 +22,27 @@ app.get('/users', (req: Request, res: Response) => {
 	res.status(200).json(users);
 });
 
+type CreateUserBody = {
+	name: string;
+};
+
+app.post('/users/', (req: Request<{}, {}, CreateUserBody>, res: Response) => {
+	const name = req.body.name;
+
+	if (!name || name.trim().length === 0) {
+		return res.status(400).json({ error: 'Name is required' });
+	}
+
+	const newUser: User = {
+		id: users.length ? users[users.length - 1].id + 1 : 1,
+		name: name.trim(),
+	};
+
+	users.push(newUser);
+
+	res.status(201).json(newUser);
+});
+
 app.get('/users/:id', (req: Request, res: Response) => {
 	const id = Number(req.params.id);
 
@@ -33,6 +54,37 @@ app.get('/users/:id', (req: Request, res: Response) => {
 
 	res.status(200).json(user);
 });
+
+type UpdateUserBody = {
+	name?: string;
+};
+
+app.patch(
+	'/users/:id',
+	(req: Request<{ id: string }, {}, UpdateUserBody>, res: Response) => {
+		const id = Number(req.params.id);
+
+		const user = users.find((u) => u.id === id);
+		if (!user) {
+			return res.status(404).json({ error: 'User does not exist!' });
+		}
+
+		const name = req.body?.name;
+		if (name === undefined) {
+			return res.status(400).json({ error: 'Nothing to update' });
+		}
+
+		if (typeof name !== 'string' || name.trim().length === 0) {
+			return res
+				.status(400)
+				.json({ error: 'Name must be a non-empty string' });
+		}
+
+		user.name = name.trim();
+
+		return res.status(200).json(user);
+	},
+);
 
 const PORT = 3000;
 app.listen(PORT, () => {
